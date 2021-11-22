bedf2ba5ffe4750d30c828fc92212a284e1107eb
thisisazeez <imfostersyndrome@gmail.com>
2021-11-22 11:42:37 +0100

    Fixing Database Connection line 25 in db.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -22,7 +22,7 @@ def close_db(e=None):
 def init_db():
     db = get_db()
     with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf-8'))
+        db.executescript(f.read().decode('utf-8')) # errors = "ignore" not working
 
 
 @click.command('init-db')
